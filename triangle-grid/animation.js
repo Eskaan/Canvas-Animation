4d04db336582fd069ff13fac0ddbf1393bcf4815
Code-Ac <76821666+Code-Ac@users.noreply.github.com>
2021-09-07 13:55:29 +0200

    Moved back to demo file and animation file

--- triangle-grid/animation.js
+++ triangle-grid/animation.js
@@ -1,5 +1,5 @@
 /**
- * Starts a new Animation in #
+ * Starts a new Animation in `#animationCanvas`
  * @param ms interval the frames are rendered - milliseconds
  * @param npp (Nodes Per Pixel) a value of nodes created per pixel in the canvas. <br/>**< 0.0001 recommended!**
  * @param speed speed in px the nodes move per frame
@@ -9,28 +9,29 @@
  * @returns id of renderer thread - use *clearInterval(<id>)* to stop
  */
 function startAnimation(
-  ms = 100,
-  npp = .000025,
-  speed = 4,
-  range = 200,
-  opacity = .05,
-  color = (node, nodeN) => { //just an example
-    return Math.abs(Math.floor(Math.tanh((node.x - nodeN.x) / (node.y - nodeN.y)) * 360)) + 50
-  }
+    ms = 100,
+    npp = .00004,
+    speed = 5,
+    range = 200,
+    opacity = .1,
+    color = (node, nodeN) => { //just an example
+      return Math.abs(Math.floor(Math.tanh(node.x - nodeN.x / node.y - nodeN.y) * 360)) + 50;
+    }
 ) {
-  const canvas = document.getElementById('animationCanvas');
-  const ctx = canvas.getContext('2d');
+  const canvas = document.getElementById("animationCanvas");
+  const ctx = canvas.getContext("2d");
   const height = window.innerHeight;
   const width = window.innerWidth;
   const nodes = [];
 
-  canvas.addEventListener('click', () => canvas.requestFullscreen());
+  canvas.addEventListener("click", () => canvas.requestFullscreen());
 
   function newNode() {
     return {
       x: Math.random() * width,
       y: Math.random() * height,
       angle: Math.random() * 360,
+      color: Math.random() * 360
     };
   }
 
@@ -39,33 +40,39 @@ function startAnimation(
     nodes.push(newNode());
   }
 
-  ctx.fillStyle = 'rgb(0,0,0)';
+  ctx.fillStyle = "rgb(0,9,9)";
   ctx.fillRect(0, 0, width, height);
-  canvas.setAttribute('width', width.toString(10));
-  canvas.setAttribute('height', height.toString(10));
+  canvas.setAttribute("width", width.toString(10));
+  canvas.setAttribute("height", height.toString(10));
 
-  return setInterval(() => {
+  let intervals = [];
+  intervals.push(setInterval(() => {
     ctx.fillStyle = `rgba(0,0,0,${opacity})`;
     ctx.fillRect(0, 0, width, height);
-    nodes.forEach(node => {
-      nodes.filter(nodeN => nodeN !== node).forEach(nodeN => {
-        const dx = node.x - nodeN.x;
-        const dy = node.y - nodeN.y;
-        const distance = Math.sqrt(dx * dx + dy * dy);
-        if (distance <= range) {
-          ctx.beginPath();
-          ctx.moveTo(node.x, node.y);
-          ctx.lineTo(nodeN.x, nodeN.y);
-          //const color = correctColor(Math.abs(Math.floor(Math.tanh(dx / dy) * colorBandWidth)) + colorSet);
-          ctx.strokeStyle = `hsla(${color(node, nodeN)}, 100%, 50%, ${Math.abs(distance / range - 1)})`;
-          ctx.stroke();
-        }
-      });
-    });
+  }, ms));
+  nodes.forEach((node, index) => {
+    intervals.push(setInterval((node, index) => {
+      //by filtering reducing drawing functions by 50% (performance)
+      nodes.filter((nodeN, indexN) => index > indexN)
+          .forEach((nodeN) => {
+
+            const dx = node.x - nodeN.x;
+            const dy = node.y - nodeN.y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+            if (distance <= range) {
+              ctx.beginPath();
+              ctx.moveTo(node.x, node.y);
+              ctx.lineTo(nodeN.x, nodeN.y);
+              //ctx.strokeStyle = correctColor(Math.abs(Math.floor(Math.tanh(dx / dy) * colorBandWidth)) + colorSet);
+              ctx.strokeStyle = `hsla(${color(node, nodeN)}, 100%, 50%, ${Math.abs(distance / range - 1)})`;
+              ctx.stroke();
+            }
+          });
+    }, ms, node, index));
     //separate function for parallel lines
-    nodes.forEach(node => {
+    intervals.push(setInterval(node => {
       if (node.x < 0 || node.y < 0
-        || node.x > width || node.y > height) {
+          || node.x > width || node.y > height) {
         //node.angle = correctAngle(node.angle + 90);
         node.x = Math.random() * width;
         node.y = Math.random() * height;
@@ -73,6 +80,7 @@ function startAnimation(
       }
       node.x += Math.cos(node.angle * (2 * Math.PI) / 360) * speed;
       node.y += Math.sin(node.angle * (2 * Math.PI) / 360) * speed;
-    });
-  }, ms);
+    }, ms, node));
+  });
+  return intervals;
 }
